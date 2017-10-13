9522a8796c15e3d8544df017db8ac8011886b1ff
Aditya Dutta <aditya.dutta10@gmail.com>
2017-10-13 12:36:47 -0400

    initial

---
+++ Assignment2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int op;
+                char ch;
+                double x1, y1, x2, y2, scalar;
+                Console.Write("Enter x component of vector 1: ");
+                x1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter y component of vector 1: ");
+                y1 = Convert.ToDouble(Console.ReadLine());
+                Vector2D vector = new Vector2D(x1, y1);
+
+                do
+                {
+                    Console.WriteLine("Vector2D x component : " + vector.X + "\nVector2D y component : " + vector.Y);
+                    Console.WriteLine("Vector2D\n1.NegateX\n2.NegateY\n3.Add Vectors\n4.Subtract Vectors\n5.Scalar Multiplication\n6.Get Magnitude\n7.Get Angle in Degrees\n8.Quit");
+                    Console.WriteLine("\nEnter your choice: ");
+                    op = Convert.ToInt32(Console.ReadLine());
+                    switch (op)
+                    {
+                        case 1:
+                            vector.NegateX();
+                            break;
+                        case 2:
+                            vector.NegateY();
+                            break;
+                        case 3:
+                            Console.Write("Enter x component of vector 2: ");
+                            x2 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Enter y component of vector 2: ");
+                            y2 = Convert.ToDouble(Console.ReadLine());
+                            vector.AddVector(x2, y2);
+                            break;
+                        case 4:
+                            Console.Write("Enter x component of vector 2: ");
+                            x2 = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Enter y component of vector 2: ");
+                            y2 = Convert.ToDouble(Console.ReadLine());
+                            vector.SubtractVector(x2, y2);
+                            break;
+                        case 5:
+                            Console.Write("Enter scalar value for multiplication: ");
+                            scalar = Convert.ToDouble(Console.ReadLine());
+                            vector.ScalarMultiplication(scalar);
+                            break;
+                        case 6:
+                            Console.WriteLine("Magnitude: " + vector.GetMagnitude());
+                            break;
+                        case 7:
+                            Console.WriteLine("Angle: " + vector.GetAngleInDegrees());
+                            break;
+                        case 8:
+                            System.Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Input! Try again.");
+                            break;
+                    }
+                    Console.WriteLine("Continue?(y/n)");
+                    ch = Convert.ToChar(Console.ReadLine());
+                }
+                while (ch == 'y' || ch == 'Y');
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error encountered: " + e.Message + "\nRun the program again! Press any key.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Assignment2/Vector2D.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Vector2D
+    {
+
+        //Declaring two variables to store vector 1
+        private double x;
+        private double y;
+
+        //getter and setter for x
+        public double X
+        {
+            get{ return x; }
+            set { x = value; }
+        }
+        //getter and setter for y
+        public double Y
+        {
+            get { return y; }
+            set { x = value; }
+        }
+        //constructor to initialize object of class
+        public Vector2D(double xValue, double yValue)
+        {
+            x = xValue;
+            y = yValue;
+        }
+
+        //flipping sign for x
+        public void NegateX()
+        {
+            Console.WriteLine("NegateX : " + (x *= -1));
+        }
+
+        //flipping sign for y
+        public void NegateY()
+        {
+           Console.WriteLine("NegateY : " + (y *= -1));
+        }
+
+        //vector addition
+        public void AddVector(double xValue, double yValue)
+        {
+            Console.WriteLine("Vector Addition: [" + (xValue + x) + "," + (yValue + y) + "]");
+        }
+
+        //vector subtraction
+        public void SubtractVector(double xValue, double yValue)
+        {
+            Console.WriteLine("Vector Subtraction: [" + (xValue - x) + "," + (yValue - y) + "]");
+        }
+
+        //scalar multiplication 
+        // scalar * [x , y]
+        public void ScalarMultiplication(double scalar)
+        {
+            Console.WriteLine("Scalar Multiplication: [" + (x *= scalar) + "," + (y *= scalar) + "]");          
+        }
+
+        //Getting magnitude using pythagorean theorem
+        // r^2 = a^2 + b^2
+        public double GetMagnitude()
+        {
+           
+            return (Math.Sqrt(x*x + y*y));
+        }
+
+        //Getting angle in radians and converting it into degrees using 180/PI
+        public double GetAngleInDegrees()
+        {
+            if (x > 0 && y > 0)
+                Console.Write("The coordinate point ({0} , {1}) lies in the First quandrant.\n\n", x, y);
+            else if (x < 0 && y > 0)
+                Console.Write("The coordinate point ({0} , {1}) lies in the Second quandrant.\n\n", x, y);
+            else if (x < 0 && y < 0)
+                Console.Write("The coordinate point ({0} , {1}) lies in the Third quandrant.\n\n", x, y);
+            else if (x > 0 && y < 0)
+                Console.Write("The coordinate point ({0} , {1}) lies in the Fourth quandrant.\n\n", x, y);
+            else if (x == 0 && y == 0)
+                Console.Write("The coordinate point ({0} , {1}) lies at the origin.\n\n", x, y);
+            return ((Math.Atan(y / x)) * (180/Math.PI));
+        }
+
+    }
+}
